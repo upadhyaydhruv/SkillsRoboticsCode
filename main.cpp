c5f13d2b15c25ec5678314db1ed97a7a23f06574
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-03-08 15:30:10 -0400

    Added robot path to first, second, and third tees

--- main.cpp
+++ main.cpp
@@ -113,14 +113,13 @@ int main() {
   // for testing; remove this after implementing picking up automatically
   pu.pickUp();
 
-  while (true) {
-    // read the sensors
-    bool leftOn = ls.leftOn();
-    bool rightOn = ls.rightOn();
-    bool middleOn = ls.middleOn();
-  
+  while (true) {  
     // check if it's at the end, if so, do an action (blocking, i.e. wait for it to finish), and "continue" the loop
     while (true){
+      // read the sensors
+      bool leftOn = ls.leftOn();
+      bool rightOn = ls.rightOn();
+      bool middleOn = ls.middleOn();
       while (leftOn&&!middleOn){
         Drivetrain.setTurnVelocity(10, rpm);
         Drivetrain.turn(right);
@@ -149,6 +148,10 @@ int main() {
       Drivetrain.turnFor(180, degrees);
 
     while (true){
+      // read the sensors
+      bool leftOn = ls.leftOn();
+      bool rightOn = ls.rightOn();
+      bool middleOn = ls.middleOn();
       while (leftOn&&!middleOn){
         Drivetrain.setTurnVelocity(10, rpm);
         Drivetrain.turn(right);
@@ -171,7 +174,122 @@ int main() {
     //Assuming in front of tee, put the ball down.
     pu.putDown();
 
-    Drivetrain.turnFor(-90, degrees); //Turning left towards line leading to the next tee
+    //At this point, the first ball should have been placed in the first green
+
+    Drivetrain.turnFor(90, degrees); //Turning right towards line leading to the next tee
+    // read the sensors
+    bool leftOn = ls.leftOn();
+    bool rightOn = ls.rightOn();
+    bool middleOn = ls.middleOn();
+    while (!leftOn||!rightOn||!middleOn){
+      Drivetrain.drive(forward);
+    }
+
+    while (true) {
+      // read the sensors
+      bool leftOn = ls.leftOn();
+      bool rightOn = ls.rightOn();
+      bool middleOn = ls.middleOn();
+      while (!middleOn&&leftOn){
+        Drivetrain.turn(right);
+      }
+      while(!middleOn&&rightOn){
+        Drivetrain.turn(left);
+      }
+      if (!leftOn&&!middleOn&&!rightOn)
+        break;
+    }
+
+    while (true){
+      // read the sensors
+      bool leftOn = ls.leftOn();
+      bool rightOn = ls.rightOn();
+      bool middleOn = ls.middleOn();
+
+      while (!leftOn||!middleOn||!rightOn) Drivetrain.drive(forward);
+      Drivetrain.turn(left); //Repositions robot to the left of the line leading to second green
+
+      while(!rightOn){ //Robot turns right until it finds line again
+        Drivetrain.turn(right);
+      }
+
+      while (true) { //Robot continues to follow line until it encounters horizontal line and 2nd green
+        // read the sensors
+        bool leftOn = ls.leftOn();
+        bool rightOn = ls.rightOn();
+        bool middleOn = ls.middleOn();
+
+        while (leftOn&&!middleOn){
+        Drivetrain.setTurnVelocity(10, rpm);
+        Drivetrain.turn(right);
+      }
+
+      while (rightOn&&!middleOn){
+        Drivetrain.setTurnVelocity(10, rpm);
+        Drivetrain.turn(left);
+      }
+
+      while(middleOn){
+        Drivetrain.setDriveVelocity(10, rpm);
+        Drivetrain.drive(forward);
+      }
+      if (leftOn && middleOn && rightOn) {
+          break;
+        }
+      }
+
+      if (pu.getIsUp()) {
+          // THIS SHOULD NEVER HAPPEN, show an error
+          Brain.Screen.print("ERROR - pickerUpper is up");
+          pu.putDown();
+      pu.pickUp();
+
+      Drivetrain.turnFor(180, degrees);
+
+    while (true){
+      // read the sensors
+      bool leftOn = ls.leftOn();
+      bool rightOn = ls.rightOn();
+      bool middleOn = ls.middleOn();
+      
+      while (leftOn&&!middleOn){
+        Drivetrain.setTurnVelocity(10, rpm);
+        Drivetrain.turn(right);
+      }
+
+      while (rightOn&&!middleOn){
+        Drivetrain.setTurnVelocity(10, rpm);
+        Drivetrain.turn(left);
+      }
+
+      while(middleOn){
+        Drivetrain.setDriveVelocity(10, rpm);
+        Drivetrain.drive(forward);
+      }
+      if (leftOn && middleOn && rightOn) {
+        break;
+      }
+    }
+
+    //Assuming in front of tee, put the ball down.
+    pu.putDown();
+
+    //At this point, the second ball should have been placed in the second green
+
+    Drivetrain.turnFor(-90, degrees);
+
+    while(leftOn||rightOn||middleOn){
+      Drivetrain.drive(forward); //Robot drives until it finds the line leading to third tee
+    }
+
+    Drivetrain.turnFor(90, degrees); //Robot turns right towards the tee
+     
+    //CONTINUE WORK FROM HERE!!
+      }
+
+    }
+
+    
 
       // for testing:
       Drivetrain.driveFor(5, inches);
@@ -200,4 +318,3 @@ int main() {
     }
   }
 }
-
